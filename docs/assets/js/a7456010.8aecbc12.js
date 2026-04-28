8bc11125b0c7268aefeddac83a712186628d1e3d
accaderi <127489013+accaderi@users.noreply.github.com>
2026-04-28 08:41:32 +0200

    Add files via upload

---
+++ docs/assets/js/a7456010.8aecbc12.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmarkd_docs=self.webpackChunkmarkd_docs||[]).push([["56"],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
